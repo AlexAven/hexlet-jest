e77ef8459a52c128fb934e31ef0ca2afe1019567
Alexander Kazakov <gloom31@gmail.com>
2024-07-11 23:54:35 +0300

    tests added

---
+++ __tests__/stack.test.js
@@ -0,0 +1,10 @@
+import makeStack from '../src/stack.js';
+
+test("stack's main flow", () => {
+  const stack = makeStack();
+  // Добавляем два элемента в стек и затем извлекаем их
+  stack.push('one');
+  stack.push('two');
+  expect(stack.pop()).toEqual('two');
+  expect(stack.pop()).toEqual('one');
+});
